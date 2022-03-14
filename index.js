8f7cc86556c511ab0c491ed1ef5f20ddb3766578
estagent <estagent@gmail.com>
2022-03-14 23:03:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+export default class {
+  __secret = null
+  cipher = null
+  config = {
+    key: 'token',
+  }
+
+  constructor(cipher, props = {}) {
+    this.cipher = cipher
+    for (let key of Object.keys(props))
+      if (this.config.hasOwnProperty(key)) this.config[key] = props[key]
+      else throw `unknown token config option (${key})`
+  }
+
+  get secret() {
+    if (!this.__secret) this.__secret = this.getSecret()
+    return this.__secret
+  }
+
+  set secret(data) {
+    if (typeof data !== 'object') throw 'input must be object'
+    if (this.cipher.validate(data) !== true) throw 'data invalid'
+    this.saveCredentials(this.cipher.encodeCredentials(data))
+    this.__secret = data.secret
+  }
+
+  delete() {
+    this.__secret = null
+    this.removeToken()
+  }
+
+  getSecret() {
+    const credentials = this.getCredentials()
+    if (credentials) {
+      const data = this.cipher.decodeCredentials(credentials)
+      if (data) {
+        if (this.cipher.validate(data) === true) return data.secret
+        else {
+          this.removeToken()
+          return null
+        }
+      }
+    }
+  }
+
+  getCredentials() {
+    return localStorage.getItem(this.config.key)
+  }
+
+  saveCredentials(string) {
+    localStorage.setItem(this.config.key, string)
+  }
+
+  removeToken() {
+    localStorage.removeItem(this.config.key)
+  }
+}
